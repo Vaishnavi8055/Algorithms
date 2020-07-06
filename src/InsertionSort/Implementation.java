7eb45f39d3fc0ed5fc7cbe2598b282871674a06f
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-07 05:23:27 +1400

    compareKey contains a value in unsorted part which will be compared by the sorted part.

--- src/InsertionSort/Implementation.java
+++ src/InsertionSort/Implementation.java
@@ -5,5 +5,9 @@ public class Implementation {
     public void InsertionSort(int array[]) {
         int arrayLength = array.length;
 
+        for(int index = 1 ; index < arrayLength ; index++) {
+            int compareKey = array[index];
+        }
+
     }
 }
