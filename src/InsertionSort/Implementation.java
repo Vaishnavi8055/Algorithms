433aeb7c4fce2fd3d3aa943143a333603f1fe978
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-07 22:27:57 +1400

    InsertionSort Implementation done

--- src/InsertionSort/Implementation.java
+++ src/InsertionSort/Implementation.java
@@ -5,14 +5,15 @@ public class Implementation {
     public void InsertionSort(int array[]) {
         int arrayLength = array.length;
 
-        for(int index = 1 ; index < arrayLength ; index++) {
+        for (int index = 1; index < arrayLength; index++) {
             int compareKey = array[index];
             int secondIndex = index - 1;
 
-            while(secondIndex >= 0 && compareKey < array[secondIndex]){
-
+            while (secondIndex >= 0 && compareKey < array[secondIndex]) {
+                array[secondIndex + 1] = array[secondIndex];
+                secondIndex--;
             }
+            array[secondIndex + 1] = compareKey;
         }
-
     }
 }
