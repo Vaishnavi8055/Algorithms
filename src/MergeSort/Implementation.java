6ba433aa89584661a4e20cce5524d35e57cd90ca
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-17 06:09:21 +1400

    Length of right and left sub array will be placed in these int type variables.

--- src/MergeSort/Implementation.java
+++ src/MergeSort/Implementation.java
@@ -2,7 +2,8 @@ package MergeSort;
 
 public class Implementation {
 
-        void mergeArray(int array[] , int leftElement , int rightElement , int middle){
-
-        }
+    void mergeArray(int array[], int leftElement, int rightElement, int middleElement) {
+        int lengthOfLeftSubArray = middleElement - leftElement + 1;
+        int LengthOfRightSubArray = rightElement - middleElement;
+    }
 }
