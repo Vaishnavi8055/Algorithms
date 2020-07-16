0160647c84fd74ac87cb49a07c7cfbfd0c003d95
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-17 06:21:05 +1400

    Copy Subarray in temporary array named LeftSubArray

--- src/MergeSort/Implementation.java
+++ src/MergeSort/Implementation.java
@@ -2,11 +2,16 @@ package MergeSort;
 
 public class Implementation {
 
-    void mergeArray(int array[], int leftElement, int rightElement, int middleElement) {
-        int lengthOfLeftSubArray = middleElement - leftElement + 1;
-        int lengthOfRightSubArray = rightElement - middleElement;
+    void mergeArray(int array[], int leftIndex, int rightIndex, int middleIndex) {
+        int lengthOfLeftSubArray = middleIndex - leftIndex + 1;
+        int lengthOfRightSubArray = rightIndex - middleIndex;
 
         int LeftSubArray[] = new int[lengthOfLeftSubArray];
         int RightSubArray[] = new int[lengthOfRightSubArray];
+
+        for (int index1 = 0; index1 < lengthOfLeftSubArray; index1++) {
+            LeftSubArray[index1] = array[leftIndex + index1];
+        }
+
     }
 }
