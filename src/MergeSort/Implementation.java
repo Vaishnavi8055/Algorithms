b08bffdbaa819517705d1f74c0ff9ab7b4cb45eb
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-17 06:10:52 +1400

    Right and Left SubArray declared.

--- src/MergeSort/Implementation.java
+++ src/MergeSort/Implementation.java
@@ -4,6 +4,9 @@ public class Implementation {
 
     void mergeArray(int array[], int leftElement, int rightElement, int middleElement) {
         int lengthOfLeftSubArray = middleElement - leftElement + 1;
-        int LengthOfRightSubArray = rightElement - middleElement;
+        int lengthOfRightSubArray = rightElement - middleElement;
+
+        int LeftSubArray[] = new int[lengthOfLeftSubArray];
+        int RightSubArray[] = new int[lengthOfRightSubArray];
     }
 }
