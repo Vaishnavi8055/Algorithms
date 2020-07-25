34b38d85f81ffe2cf682d95d48805ad16ba33b1d
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-26 04:35:05 +1400

    Implementation of Bubble Sort Algorithm

--- src/BubbleSort/Implementation.java
+++ src/BubbleSort/Implementation.java
@@ -6,11 +6,18 @@ public class Implementation {
         int lengthOfArray = array.length;
         for (int i = 0; i < lengthOfArray - 1; i++) {
             for (int j = 0; j < lengthOfArray - i - 1; j++) {
-                   if(array[j] > array[i]){
-
-                   }
+                if (array[j] > array[i]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
             }
         }
+    }
 
+    public void displaySortedArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
