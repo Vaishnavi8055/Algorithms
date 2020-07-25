2f4ebfcfde8aa42b6a6a056001ec4a0478a7c669
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-26 04:29:09 +1400

    if element at index j is greater than element at index j+1 swap the elements to sort the array

--- src/BubbleSort/Implementation.java
+++ src/BubbleSort/Implementation.java
@@ -4,7 +4,13 @@ public class Implementation {
 
     public void BubbleSort(int array[]) {
         int lengthOfArray = array.length;
+        for (int i = 0; i < lengthOfArray - 1; i++) {
+            for (int j = 0; j < lengthOfArray - i - 1; j++) {
+                   if(array[j] > array[i]){
 
+                   }
+            }
+        }
 
     }
 }
