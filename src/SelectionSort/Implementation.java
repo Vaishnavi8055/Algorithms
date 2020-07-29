8b9765540398c084bc59beb14c2da40470f2a1a2
Vaishnavi <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-30 05:59:54 +1400

    Update Implementation.java

--- src/SelectionSort/Implementation.java
+++ src/SelectionSort/Implementation.java
@@ -6,14 +6,12 @@ public class Implementation {
         int length = array.length;
         for (int i = 0; i < length - 1; i++) {
             int min_index = i;
-            for (int j = i + 1; j < length; j++) {
-                if (array[j] < array[min_index]) {
+            for (int j = i + 1; j < length; j++) 
+                if (array[j] < array[min_index]) 
                     min_index = j;
                     int temp = array[min_index];
                     array[min_index] = array[i];
                     array[i] = temp;
-                }
-            }
         }
     }
 
