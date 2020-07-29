800295ab5e5c4f49d6d89b43c21ae8970233681c
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-30 05:12:17 +1400

    compare element of array with the next element using if

--- src/SelectionSort/Implementation.java
+++ src/SelectionSort/Implementation.java
@@ -4,8 +4,13 @@ public class Implementation {
 
     public void SelectionSort(int array[]) {
         int length = array.length;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length - 1; i++) {
             int min_index = i;
+            for (int j = i + 1; j < length; j++) {
+                if (array[j] < array[min_index]) {
+
+                }
+            }
         }
     }
 }
