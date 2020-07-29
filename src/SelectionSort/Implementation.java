79c9d2aa50a5ebbc9b351c5c57a5c3fc77a6c18a
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-30 05:04:47 +1400

    min_index is a variable contains an index of minimum element

--- src/SelectionSort/Implementation.java
+++ src/SelectionSort/Implementation.java
@@ -5,7 +5,7 @@ public class Implementation {
     public void SelectionSort(int array[]) {
         int length = array.length;
         for (int i = 0; i < length; i++) {
-
+            int min_index = i;
         }
     }
 }
